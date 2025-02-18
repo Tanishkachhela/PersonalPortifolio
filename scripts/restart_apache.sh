c619aab9657c3bcd4647ad8377062a6070080418
Tanish kahhela <110801521+Tanishkachhela@users.noreply.github.com>
2025-02-18 21:19:35 +0530

    Update restart_apache.sh

--- scripts/restart_apache.sh
+++ scripts/restart_apache.sh
@@ -1,14 +1,4 @@
-version: 0.0
-os: linux
-files:
-  - source: /
-    destination: /var/www/html
-permissions:
-  - object: /var/www/html
-    owner: ec2-user
-    group: ec2-user
-hooks:
-  AfterInstall:
-    - location: scripts/restart_apache.sh
-      timeout: 300
-      runas: root
+#!/bin/bash
+echo "Restarting Apache..."
+systemctl restart httpd
+echo "Apache restarted successfully."
